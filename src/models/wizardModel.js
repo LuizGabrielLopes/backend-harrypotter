7c79bf88cb4f1830b8ed8759eb0a348a5691f2f4
LuizGabrielLopes <luiz.gabriellopesc@gmail.com>
2025-04-08 10:38:29 -0300

    feat: filtro por nome

--- src/models/wizardModel.js
+++ src/models/wizardModel.js
@@ -1,51 +1,69 @@
 const pool = require("../config/database");
 
-const getWizards = async () => {
+const getWizards = async (name) => {
+  if (!name) {
     const result = await pool.query(
-        `SELECT wizards.*, houses.name AS house_name 
+      `SELECT wizards.*, houses.name AS house_name 
         FROM wizards 
         LEFT JOIN houses ON wizards.house_id = houses.id`
     );
     return result.rows;
+  } else {
+    const result = await pool.query(
+      `
+            SELECT wizards.*, houses.name AS house_name 
+        FROM wizards 
+        LEFT JOIN houses ON wizards.house_id = houses.id WHERE wizards.name ILIKE $1`,
+      [`%${name}%`]
+    );
+    return result.rows;
+  }
 };
 
 const getWizardById = async (id) => {
-    const result = await pool.query(
-        `SELECT wizards.*, houses.name AS house_name 
+  const result = await pool.query(
+    `SELECT wizards.*, houses.name AS house_name 
         FROM wizards 
         LEFT JOIN houses ON wizards.house_id = houses.id 
-        WHERE wizards.id = $1`, [id]
-    );
-    return result.rows[0];
+        WHERE wizards.id = $1`,
+    [id]
+  );
+  return result.rows[0];
 };
 
 const createWizard = async (name, house_id) => {
-    const result = await pool.query(
-        "INSERT INTO wizards (name, house_id) VALUES ($1, $2) RETURNING *",
-        [name, house_id]
-    );
-    return result.rows[0];
+  const result = await pool.query(
+    "INSERT INTO wizards (name, house_id) VALUES ($1, $2) RETURNING *",
+    [name, house_id]
+  );
+  return result.rows[0];
 };
 
 const updateWizard = async (id, name, house_id) => {
-    const result = await pool.query(
-        `UPDATE wizards 
+  const result = await pool.query(
+    `UPDATE wizards 
         SET name = $1, house_id = $2 
         WHERE id = $3 
          RETURNING *`,
-        [name, house_id, id]
-    );
-    return result.rows[0];
+    [name, house_id, id]
+  );
+  return result.rows[0];
 };
 
 const deleteWizard = async (id) => {
-    const result = await pool.query(
-        `DELETE FROM wizards 
+  const result = await pool.query(
+    `DELETE FROM wizards 
         WHERE id = $1 
          RETURNING *`,
-        [id]
-    );
-    return result.rows[0];
+    [id]
+  );
+  return result.rows[0];
 };
 
-module.exports = { getWizards, getWizardById, createWizard, updateWizard, deleteWizard };
+module.exports = {
+  getWizards,
+  getWizardById,
+  createWizard,
+  updateWizard,
+  deleteWizard,
+};
